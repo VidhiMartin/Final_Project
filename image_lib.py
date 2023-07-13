87d17225942a93ae44b8d1c8d8eabec39a0bcc97
Vidhi Martin <Vidhi.Martin@flemingcollege.ca>
2023-07-12 20:53:12 -0400

    image_lib.py almost completed except for one function
    set_desktop_background_image(image_path) function is not completed in this script... Everything else is working just fine!

--- image_lib.py
+++ image_lib.py
@@ -1,9 +1,38 @@
 '''
 Library of useful functions for working with images.
 '''
+import requests
 def main():
     # TODO: Add code to test the functions in this module
-    return
+
+    #for testing download image function
+    image_url = 'https://apod.nasa.gov/apod/image/2307/Ngc1398_Hanson_2752.jpg'
+    image_data = download_image(image_url)
+    if image_data is not None:
+        print('Image retrieved successfully!')
+    else:
+        print('Failed to retrieve image!')
+    
+    #for testing save image function
+    image_path = "C:\Users\16476\Documents\Scripting Applications\Final_Project"
+    saved = save_image_file(image_data, image_path)
+    if saved:
+        print("Imaged saved successfully!")
+    else:
+        print("Couldn't save image!")
+
+    #for testing background function
+    saved_in_background = set_desktop_background_image(image_path)
+    if saved_in_background:
+        print("Desktop background changed successfully!")
+    else:
+        print("Couldn't set the desktop background image!")
+
+    #for testing the scale function
+    image_size = (1200, 800)
+    max_size = (800, 600)
+    scaled_image = scale_image(image_size, max_size)
+    print(f"The size of the image is {scaled_image}.")
 
 def download_image(image_url):
     """Downloads an image from a specified URL.
@@ -17,7 +46,16 @@ def download_image(image_url):
         bytes: Binary image data, if succcessful. None, if unsuccessful.
     """
     # TODO: Complete function body
-    return
+    try:
+        response = requests.get(image_url)
+        if response.ok:
+            return response.content
+        else:
+            print('Please try again!')
+    except requests.RequestException as e:
+        print('Error Occurred:', e)
+            
+    return None
 
 def save_image_file(image_data, image_path):
     """Saves image data as a file on disk.
@@ -32,7 +70,12 @@ def save_image_file(image_data, image_path):
         bool: True, if succcessful. False, if unsuccessful
     """
     # TODO: Complete function body
-    return
+    try:
+        with open(image_path, 'wb') as file:
+            file.write(image_data)
+        return None
+    except OSError as e:
+        print("Error: Couldn't save image", e)
 
 def set_desktop_background_image(image_path):
     """Sets the desktop background image to a specific image.
